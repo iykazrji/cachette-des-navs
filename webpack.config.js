443b3215322fc6d5687543d68bf01ebebac05b45
iyk-azorji <iykazorji@gmail.com>
2018-01-15 14:40:59 +0100

    Fixed sock.js issues. Should help fix hot-reloading issues

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:3000', // WebpackDevServer host and port
+    'webpack-dev-server/client?http://localhost:8080', // WebpackDevServer host and port
     'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
     './src/index.js'
   ],
