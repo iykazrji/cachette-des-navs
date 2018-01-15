44bd0c2a6ad76cb40c95c8fe88f17c75b3d6a433
iyk-azorji <iykazorji@gmail.com>
2018-01-15 14:38:03 +0100

    Fixed issues with Webpack and Nav rendering

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,67 +4,99 @@ import PropTypes from 'prop-types'
 import MainStyles from '../styles/styles'
 
 export default class Navbar extends Component{
-	constructor(props){
-		super(props)
-		this.state = {
-			navHeight: null,
-			lastScrollTop: 0,
-			delta: 5,
-			isNavVisible: true
-		}
-		this.hasScrolled = this.hasScrolled.bind(this)
-		this.renderNavbar = this.renderNavbar.bind(this)
-		this.checkNavVisibility = this.checkNavVisibility.bind(this)
-	}
-	componentDidMount(){
-		this.setState({
-			navHeight: this.node.offsetHeight,
-		})
-		this.checkNavVisibility()
+	constructor(props) {
+    super(props);
+    this.state = {
+      navHeight: null,
+      lastScrollTop: 0,
+      delta: 5,
+      isNavVisible: true,
+      shouldNavVisible: false
+    };
 
-	}
-	componentWillUnmount(){
-		// Clear the intervals, timeouts and EventListeners we s we'd Create in the
-		// ComponentDidMount Method
-		clearInterval(this.checkNavVisibility)
-	}
+    this.hasScrolled = this.hasScrolled.bind(this);
+    this.renderNavbar = this.renderNavbar.bind(this);
+    this.checkNavVisibility = this.checkNavVisibility.bind(this);
+    this.checkPageScrolledPastTop = this.checkPageScrolledPastTop.bind(this);
+  }
+  componentDidMount() {
+    this.setState({
+      navHeight: this.node.offsetHeight
+    });
 
-	// This will simply return an interval we can clear once we Unmount the component...
-	checkNavVisibility(){
-		return (
-			setInterval(()=>{
-				if(this.props.isScrolling){
-					this.hasScrolled()
-				}
-			}, 250)
-		)
-	}    
-	hasScrolled(){
-		let scrollTop = document.body.scrollTop
-		// Make sure User has scrolled past the Delta...
-		if(Math.abs(this.state.lastScrollTop - scrollTop) <= this.state.delta){
-			return
-		}
-		// If User scrolled down and are past the navbar, set Visible to false
-	    // This is necessary so you never see what is "behind" the navbar.
-	    if (scrollTop > this.state.lastScrollTop && scrollTop > this.state.navHeight){
-	    	// Scrolling Down
-	    	this.setState({
-	    		isNavVisible: false
-	    	})
-	    }else{
-	    	// Scrolling Up
-	    	if(scrollTop + window.innerHeight < document.body.clientHeight) {
-	            this.setState({
-	            	isNavVisible: true
-	            })
-	        }
-	    }
-	   	// Reset the LastScrollTop state to The current Scroll Position
-	    this.setState({
-	    	lastScrollTop: scrollTop
-	    })
-	}
+    window.requestAnimationFrame(this.checkNavVisibility);
+  }
+  componentWillUnmount() {
+    // Clear the intervals, timeouts and EventListeners we s we'd Create in the
+    // ComponentDidMount Method
+    cancelAnimationFrame(this.visibleCheckInterval);
+  }
+
+  // This will simply return an interval we can clear once we Unmount the component...
+  checkNavVisibility() {
+    this.checkPageScrolledPastTop();
+    if (this.props.isScrolling) {
+      this.hasScrolled();
+    }
+    this.visibleCheckInterval = window.requestAnimationFrame(
+      this.checkNavVisibility
+    );
+  }
+
+  // Check if the browser hits the top of the page
+  checkPageScrolledPastTop() {
+    let scrollTop =
+      window.pageYOffset !== undefined
+        ? window.pageYOffset
+        : (document.documentElement ||
+            document.body.parentNode ||
+            document.body).scrollTop;
+
+    if (scrollTop >= this.state.navHeight + 100) {
+      this.setState({
+        shouldNavVisible: true
+      });
+    } else {
+      this.setState({
+        shouldNavVisible: false
+      });
+    }
+  }
+
+  hasScrolled() {
+    let scrollTop =
+      window.pageYOffset !== undefined
+        ? window.pageYOffset
+        : (document.documentElement ||
+            document.body.parentNode ||
+            document.body).scrollTop;
+
+    if (Math.abs(this.state.lastScrollTop - scrollTop) <= this.state.delta) {
+      return;
+    }
+    // If User scrolled down and are past the navbar, set Visible to false
+    // This is necessary so you never see what is "behind" the navbar.
+    if (
+      scrollTop > this.state.lastScrollTop &&
+      scrollTop > this.state.navHeight
+    ) {
+      // Scrolling Down
+      this.setState({
+        isNavVisible: false
+      });
+    } else {
+      // Scrolling Up
+      if (scrollTop + window.innerHeight < document.body.clientHeight) {
+        this.setState({
+          isNavVisible: true
+        });
+      }
+    }
+    // Reset the LastScrollTop state to The current Scroll Position
+    this.setState({
+      lastScrollTop: scrollTop
+    });
+  }
 	renderNavbar(){
 		// Declare possible styles...
 		let styles = {
